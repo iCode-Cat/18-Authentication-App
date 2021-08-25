98b112a2e0bcd3f0f82d66776abdc1a2bb7e7261
iCode-Cat <devranboyac8@gmail.com>
2021-08-26 00:15:25 +0300

    Profile Update API Built

--- backend/src/routes/api/profile.js
+++ backend/src/routes/api/profile.js
@@ -1,5 +1,6 @@
 const express = require('express');
 const router = express.Router();
+const User = require('../../../models/User');
 
 // @router /api/user/profile
 // @desc Get authenticated user's profile
@@ -8,6 +9,32 @@ router.get('/', (req, res) => {
     return res.status(401).json({ msg: 'You are not authenticated.' });
   }
   res.status(200).json(req.user);
+});
+
+// @router /api/user/profile
+// @desc Update authenticated user's profile
+router.post('/', async (req, res) => {
+  console.log(req.user);
+
+  if (req.user === undefined) {
+    return res
+      .status(404)
+      .send('Authentication Failed! Please Log User In First.');
+  }
+  try {
+    const { _id } = req.user;
+    const object = {
+      name: 'JUJU',
+      phone: 121212,
+    };
+    // Find user by userid
+    const user = await User.updateOne({ _id }, { ...object });
+    res.status(200).send('User Profile Updated Successfully');
+    // Update with the given credentials
+  } catch (error) {
+    console.log(error.message);
+    res.status(500).send('Server Error');
+  }
 });
 
 module.exports = router;
